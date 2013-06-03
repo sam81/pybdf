0bce380244a1d1e08b292a7074f41d32bc6e2b01
Samuele Carcagno <sam.carcagno@gmail.com>
2013-06-03 13:54:14 +0200

    new test release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from numpy.distutils.core import Extension
 ext1 = Extension(name = 'libforbdf',
                  sources = ['libforbdf.f95'])
 setup(name="pybdf",    
-    version="0.1.9",
+    version="0.1.10",
       py_modules=["pybdf"],
       ext_modules = [ext1],
       author="Samuele Carcagno",
